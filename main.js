8a16e154a98049365c6a639a90e5157923aa43f5
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-10-27 17:10:17 +0200

    prova1

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+console.log(ciao)
 let btn = document.querySelector("#myBtn")
 btn.addEventListener("click" function(){
     document.getElementById("nome").value;
     document.getElementById("km").value;
     document.getElementById("eta").value;
+    console.log(miobottone)
 })
 
 const z = parseFloat(0.21)
@@ -11,13 +13,13 @@ const prezzo = parseFloat(km * z)
 
 if ( eta < 18) {
     prezzoScontato = parseFloat(prezzo -(prezzo * 0.2)).toFixed(2)
-   
+    document.getElementById("costo").innerHTML= prezzoScontato
 } else if(eta >= 65){
     prezzoScontato = parseFloat(prezzo -(prezzo * 0.4)).toFixed(2)
-  
+    document.getElementById("costo").innerHTML=  prezzoScontato
 } else{
     prezzoScontato = parseFloat(prezzo).toFixed(2)
-   
+    document.getElementById("costo").innerHTML= prezzo
 }
 
 document.getElementById("nomeb").innerHTML= nome
