b3b804cf83612cf4d6040e3817268f0e23593c99
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-10-27 19:39:35 +0200

    FINISCE JAVASCRIPT E INIZIA CSS

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
-console.log(ciao)
+
+
 let btn = document.querySelector("#myBtn")
-btn.addEventListener("click" function(){
-    document.getElementById("nome").value;
-    document.getElementById("km").value;
-    document.getElementById("eta").value;
-    console.log(miobottone)
+btn.addEventListener("click", function(){
+    let nome =  document.getElementById("nome").value;
+    let km = document.getElementById("km").value;
+    let eta =  document.getElementById("eta").value;
+    document.getElementById("secondo").style.display ="block"
+    
+    const z = 0.21
+    const prezzo = (km * z)
+   if  (eta == "minorenne") {
+       prezzoScontato = (prezzo -(prezzo * 0.2)).toFixed(2)
+       document.getElementById("costo").innerHTML= prezzoScontato
+       document.getElementById("tipobiglietto").innerHTML= "ridotto minorenni"
+   } else if(eta == "senior"){
+       prezzoScontato = (prezzo -(prezzo * 0.4)).toFixed(2)
+       document.getElementById("costo").innerHTML= prezzoScontato
+       document.getElementById("tipobiglietto").innerHTML= "ridotto senior" 
+   } else{
+       prezzoScontato = (prezzo).toFixed(2)
+       document.getElementById("costo").innerHTML= prezzoScontato
+       document.getElementById("tipobiglietto").innerHTML= "standard"
+   }
+    document.getElementById("nomeb").innerHTML= nome
+    document.getElementById("carrozza").innerHTML= Math.floor(Math.random()*10)+1
+
+const numeroBiglietto = 90000 + Math.floor(Math.random()*10000)
+document.getElementById("numerobiglietto").innerHTML = numeroBiglietto
 })
 
-const z = parseFloat(0.21)
-
-const prezzo = parseFloat(km * z)
-
-if ( eta < 18) {
-    prezzoScontato = parseFloat(prezzo -(prezzo * 0.2)).toFixed(2)
-    document.getElementById("costo").innerHTML= prezzoScontato
-} else if(eta >= 65){
-    prezzoScontato = parseFloat(prezzo -(prezzo * 0.4)).toFixed(2)
-    document.getElementById("costo").innerHTML=  prezzoScontato
-} else{
-    prezzoScontato = parseFloat(prezzo).toFixed(2)
-    document.getElementById("costo").innerHTML= prezzo
-}
-
-document.getElementById("nomeb").innerHTML= nome
-document.getElementById("costo").innerHTML= prezzoScontato
+let btnc = document.querySelector("#cancella")
+btnc.addEventListener("click", function(){
+    document.getElementById("nome").value= ""
+    document.getElementById("km").value = ""
+    document.getElementById("eta").value = "minorenne"
+})
